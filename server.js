7a58e97536d3e4437edcbf94536000338ba2af2e
Pankaj Kumar <71117524+PankajKumarVC@users.noreply.github.com>
2026-03-13 17:14:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
+const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
-const server = http.createServer();
-const io = new Server(server, { cors: { origin: "*" } });
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 const users = {}; // userId -> socketId
 
+app.get("/", (req, res) => {
+  res.send("NexChat Server is Live! 🚀");
+});
+
 io.on("connection", (socket) => {
 
-    socket.on("register", (userId) => {
-        users[userId] = socket.id;
+  socket.on("register", (userId) => {
+    users[userId] = socket.id;
+    io.emit("users", Object.keys(users));
+  });
 
-        // send updated user list
-        io.emit("users", Object.keys(users));
-    });
+  socket.on("private-message", ({ to, message, from }) => {
+    const receiver = users[to];
+    if (receiver) {
+      io.to(receiver).emit("private-message", { from, message });
+    }
+  });
 
-    socket.on("private-message", ({ to, message, from }) => {
-
-        const receiver = users[to];
-
-        if (receiver) {
-            io.to(receiver).emit("private-message", {
-                from,
-                message
-            });
-        }
-
-    });
-
-    socket.on("disconnect", () => {
-        for (let id in users) {
-            if (users[id] === socket.id) {
-                delete users[id];
-            }
-        }
-
-        io.emit("users", Object.keys(users));
-    });
+  socket.on("disconnect", () => {
+    for (let id in users) {
+      if (users[id] === socket.id) {
+        delete users[id];
+      }
+    }
+    io.emit("users", Object.keys(users));
+  });
 
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("NexChat Server running on port " + PORT);
 });
